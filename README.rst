0c9dc7cc4d0cd1c8c49cde6350ae1ba19b6a8e65
Daniel Suess <daniel@dsuess.me>
2017-07-03 17:28:21 +1000

    Add publishing stuff

--- README.rst
+++ README.rst
@@ -10,6 +10,11 @@ pyscgm
    :alt: Latest Travis CI build status
 
 An implementation of sketchy CGM from [1] for low-rank matrix recovery.
+The module `pyscgm.extmath` provides implementations of the randomized
+algorithms from [2] for partial eigenvalue and singular value decompositions
+for matrix-free linear algebra.
+Both work for real and complex matrices.
+The interface is based on `scipy.sparse.linalg.LinearOperator`.
 
 Usage
 -----
@@ -32,6 +37,9 @@ __[2]__ Halko, N., Martinsson, P.-G., & TroJ. A. (2009). Finding structure with 
 
 Licence
 -------
+
+This work is distributed under the terms of the BSD 3-clause license (see
+[LICENSE](LICENSE)).
 
 Authors
 -------
--- setup.py
+++ setup.py
@@ -7,16 +7,23 @@ setuptools.setup(
 
     author="Daniel Suess",
     author_email="daniel@dsuess.me",
+    license='BSD',
 
     description="Implementation of CGM using matrix sketches",
     long_description=open('README.rst').read(),
 
     packages=setuptools.find_packages(),
 
-    install_requires=[],
+    install_requires=['numpy', 'scipy'],
+    setup_requires=['pytest-runner', 'pytest-benchmark'],
+    platforms=['ALL'],
+
+    package_dir={'pycsgm': 'pycsgm'},
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
