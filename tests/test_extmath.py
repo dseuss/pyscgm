702016efdceb05cb8a909f550b8b86589f047788
Daniel Suess <daniel@dsuess.me>
2017-07-03 11:37:24 +1000

    Relative import test._utils

--- tests/test_extmath.py
+++ tests/test_extmath.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest as pt
 from numpy.testing import assert_allclose
 
-from ._utils import random_lowrank, truncated_svd, normalize_svec
+from . import _utils as u
 import pyscgm.extmath as m
 
 
@@ -18,7 +18,7 @@ def test_approximate_range_finder(rows, cols, rank, dtype, piter_normalizer, rge
     rf_size = rank + 10
     assert min(rows, cols) > rf_size
 
-    A = random_lowrank(rows, cols, rank, rgen=rgen, dtype=dtype)
+    A = u.random_lowrank(rows, cols, rank, rgen=rgen, dtype=dtype)
     A /= np.linalg.norm(A, ord='fro')
     Q = m.approx_range_finder(A, rf_size, 7, rgen=rgen,
                               piter_normalizer=piter_normalizer)
@@ -34,12 +34,11 @@ def test_approximate_range_finder(rows, cols, rank, dtype, piter_normalizer, rge
 @pt.mark.parametrize('transpose', [False, True, 'auto'])
 def test_randomized_svd(rows, cols, rank, dtype, transpose, rgen):
     rank = min(rows, cols) if rank is 'fullrank' else rank
-    A = random_lowrank(rows, cols, rank, rgen, dtype)
-    U_ref, s_ref, V_ref = truncated_svd(A, rank)
-    U, s, V = m.randomized_svd(A, rank, transpose=transpose, rgen=rgen,
-                               n_iter=10)
+    A = u.random_lowrank(rows, cols, rank, rgen, dtype)
+    U_ref, s_ref, V_ref = u.truncated_svd(A, rank)
+    U, s, V = m.randomized_svd(A, rank, transpose=transpose, rgen=rgen)
     # since singular vectors are only determined up to a phase
-    U, U_ref, V, V_ref = map(normalize_svec, (U, U_ref, V, V_ref))
+    U, U_ref, V, V_ref = map(u.normalize_svec, (U, U_ref, V, V_ref))
 
     assert_allclose(np.linalg.norm(U - U_ref, axis=0), 0, atol=1e-3)
     assert_allclose(np.linalg.norm(V - V_ref, axis=0), 0, atol=1e-3)
