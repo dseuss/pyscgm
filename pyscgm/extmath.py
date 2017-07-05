cde789613b39ee6ec234c37e1e7e7f92b06e601c
Daniel Suess <daniel@dsuess.me>
2017-07-05 17:10:57 +1000

    Add standard_normal function

--- pyscgm/extmath.py
+++ pyscgm/extmath.py
@@ -3,6 +3,34 @@ from scipy import linalg
 from scipy.sparse.linalg import aslinearoperator
 
 
+def standard_normal(shape, rgen=np.random, dtype=np.float_):
+    """Generates a standard normal numpy array of given shape and dtype, i.e.
+    this function is equivalent to `rgen.randn(*shape)` for real dtype and
+    `rgen.randn(*shape) + 1.j * rgen.randn(shape)` for complex dtype.
+
+    Parameters
+    ----------
+
+    shape: tuple
+
+    rgen: RandomState (default is `np.random`))
+
+    dtype: `np.float_` (default) or `np.complex_`
+
+    Returns
+    -------
+
+    A: An array of given shape and dtype with standard normal entries
+
+    """
+    if dtype == np.float_:
+        return rgen.randn(*shape)
+    elif dtype == np.complex_:
+        return rgen.randn(*shape) + 1.j * rgen.randn(*shape)
+    else:
+        raise ValueError('{} is not a valid dtype.'.format(dtype))
+
+
 def approx_range_finder(A, sketch_size, n_iter, piter_normalizer='auto',
                         rgen=np.random):
     """Computes an orthonormal matrix whose range approximates the range of A.
@@ -52,9 +80,8 @@ def approx_range_finder(A, sketch_size, n_iter, piter_normalizer='auto',
     """
     A = aslinearoperator(A)
 
-    # Generating normal random vectors with shape: (A.shape[1], sketch_size)
-    # note that real normal vectors are sufficient, no complex values necessary
-    Q = rgen.randn(A.shape[1], sketch_size)
+    # note that real normal vectors might actually be sufficient
+    Q = standard_normal((A.shape[1], sketch_size), rgen=rgen, dtype=A.dtype)
 
     # Deal with "auto" mode
     if piter_normalizer == 'auto':
